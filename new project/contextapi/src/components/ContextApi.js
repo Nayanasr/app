7451dea8f79edeb502f8c729a139327d49af7500
Nayanasr <111118942+Nayanasr@users.noreply.github.com>
2022-09-06 11:05:54 +0530

    Delete new project directory

--- new project/contextapi/src/components/ContextApi.js
+++
@@ -1,7 +0,0 @@
-import React, { createContext } from "react";
-const contextData = createContext();
-const DataProvider = contextData.Provider;
-const DataConsumer = contextData.Consumer;
-
-export default contextData;
-export { DataProvider, DataConsumer };
--- new project/routing/src/components/about/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const About = () => {
-  return (
-    <div>About page will be rendered</div>
-  )
-};
-
-export default About;
